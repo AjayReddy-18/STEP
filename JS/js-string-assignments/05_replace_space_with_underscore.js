e453d57001a1a0db308505e6fa6e2f5e4557a3b1
Ajay Reddy <ajayrahul639@gmail.com>
2024-10-17 21:32:09 +0530

    small change

--- JS/js-string-assignments/05_replace_space_with_underscore.js
+++ JS/js-string-assignments/05_replace_space_with_underscore.js
@@ -1,4 +1,4 @@
-    // Do not rename string, use it as input for your program.
+// Do not rename string, use it as input for your program.
 // While testing we will change it's value.
 const string = "abcba jd sdj   ";
 // Replace all spaces with underscore "_"
@@ -7,11 +7,13 @@ const string = "abcba jd sdj   ";
 // START YOUR CODE AFTER THIS LINE. DO NOT REMOVE THIS LINE
 
 const stringLength = string.length;
+const SPACE = " ";
+const UNDERSCORE = "_";
 let stringWithOutSpaces = "";
 
 for (let currentIndex = 0; currentIndex < stringLength; currentIndex++) {
     const currentCharacter = string[currentIndex];
-    stringWithOutSpaces = stringWithOutSpaces + (currentCharacter === " " ? "_" : currentCharacter);
+    stringWithOutSpaces = stringWithOutSpaces + (currentCharacter === SPACE ? UNDERSCORE : currentCharacter);
 }
 
 console.log(stringWithOutSpaces);
