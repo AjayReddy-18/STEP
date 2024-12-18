347f49dc82b21afc35ba26d17e3b65e2a70ce693
Ajay Reddy <137703598+AjayReddy-18@users.noreply.github.com>
2024-12-18 09:50:32 +0530

    Delete JS directory

--- JS/js-practice/squareRoot.js
+++
@@ -1,7 +0,0 @@
-// Find square root of a number 
-
-const number = 19; 
-
-const squareRoot = number ** (1/2)
-
-console.log(squareRoot);
--- JS/js-strings/js-string-assignments-2/test.js
+++
@@ -1,9 +0,0 @@
-const add = function (a, b) {
-    return a + b;
-}
-
-function add (a, b) {
-    return a + b;
-}
-
-console.log(add); 
