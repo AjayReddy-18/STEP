6f7b29001a7beeb7acbaa38e1b2d5e47f25c8e84
Ajay Reddy <iamajayreddy18@gmail.com>
2024-12-30 18:15:47 +0530

    modified many things

---
+++ js/js-testing/objectTesting.js
@@ -0,0 +1,26 @@
+const isEqual = (lhs, rhs) => lhs === rhs;
+
+const areEqual = (lhs, rhs) => {
+  if (typeof lhs !== "object") return isEqual(lhs, rhs);
+
+  const lhsKeys = Object.keys(lhs);
+  const rhsKeys = Object.keys(rhs);
+
+  if (lhsKeys.length !== rhsKeys.length) return false;
+  
+  return lhsKeys.every((key) => key in rhs && areEqual(lhs[key], rhs[key]));
+}
+
+const runTest = (testCase) => {
+  const actual = testCase.fn(...testCase.args);
+  const STATUS = areEqual(actual, testCase.expected) ? "PASS" : "FAIL";
+  return { STATUS, ...testCase, actual };
+}
+
+export const testAll = (testCases) => {
+  const allCases = testCases.map((testCase) => runTest(testCase));
+  const passedCases = allCases.filter(({ STATUS }) => STATUS === "PASS");
+  const failedCases = allCases.filter(({ STATUS }) => STATUS === "FAIL");
+  
+  return { passedCases, failedCases, allCases };
+};
