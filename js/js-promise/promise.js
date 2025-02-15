b6be078060b61b3119c0c4afd0eb36b7e49231e5
Ajay Reddy <iamajayreddy18@gmail.com>
2025-02-15 17:51:38 +0530

    new file

--- js/js-promise/promise.js
+++ js/js-promise/promise.js
@@ -1,20 +1,50 @@
-const promise = Promise.resolve("main promise");
+// const promise = Promise.resolve("main promise");
 
-const handler = (rv) => {
-  console.log(rv);
-  return `${rv} -> then`;
-};
+// const handler = (rv) => {
+//   console.log(rv);
+//   return `${rv} -> then`;
+// };
 
-promise
-  .then((rv) => {
-    return Promise.resolve("new promise");
+// promise
+//   .then((rv) => {
+//     return Promise.resolve("new promise");
+//   })
+//   .then(handler)
+//   .then(handler);
+
+// promise
+//   .then((rv) => {
+//     return handler(rv + "-> second then");
+//   })
+//   .then(handler)
+//   .then(handler);
+
+const p1 = new Promise((resolve, reject) => {
+  resolve("Success");
+});
+
+p1.then((value) => {
+  console.log(value + "##"); // "Success!"
+  throw new Error("oh, no!##");
+})
+  .catch((e) => {
+    console.error(e.message); // "oh, no!"
   })
-  .then(handler)
-  .then(handler);
+  .then(
+    () => console.log("after a catch the chain is restored##"), // "after a catch the chain is restored"
+    () => console.log("Not fired due to the catch")
+  );
 
-promise
-  .then((rv) => {
-    return handler(rv + "-> second then");
+// The following behaves the same as above
+p1.then((value) => {
+  console.log(value); // "Success!"
+  // return Promise.reject("oh, no!");
+  throw new Error("oh, no!");
+})
+  .catch((e) => {
+    console.error(e.message); // "oh, no!"
   })
-  .then(handler)
-  .then(handler);
+  .then(
+    () => console.log("after a catch the chain is restored"), // "after a catch the chain is restored"
+    () => console.log("Not fired due to the catch")
+  );
